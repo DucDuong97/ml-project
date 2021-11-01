daf1f41ee02f938fc3104a2fce16deff08844c97
Duy Nguyen Dinh <mcaneway8469@gmail.com>
2021-11-01 15:21:03 +0100

    Implement Euclidian-Distance-Function and find-k-nearest-neighbor-function
    TODO: Is it allowed for k-nearest neighbors to contain the calculating test point itself?\nHow to get the label of the neighbors as not all points in train_x was assigned with a label?

--- src/task1/knn.py
+++ src/task1/knn.py
@@ -1,10 +1,44 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 from dataset import *
 
 
 def euclidean_distance(a, b):
-    raise NotImplementedError('TODO')
+    dist = 0.0
+    a = a.flatten()
+    b = b.flatten()
+    for i in range(len(a)):
+        dist += (a[i] - b[i]) ** 2
+    return math.sqrt(dist)
+
+
+def find_neighbors(test, X, k):
+    """
+    Find k nearest neighbors of point test
+
+    :param test: Calculating point
+    :param X:
+    :param k: Number of nearest neighbors need to find
+    :return: List of k nearest neighbors (may contain the test point itself)
+    """
+    dists = []
+    # X = np.array(X).tolist()
+    for i in X:
+        dists.append((i, euclidean_distance(test, i)))
+    dists.sort(key=lambda tup: tup[1])
+    neighbors = []
+    for i in range(k):
+        neighbors.append((dists[i][0], dists[i][1]))
+        print("Nearest neighbor with the distance ", dists[i][1])
+    return neighbors
+
+
+def label(neighbors, Y):
+    print(np.size(Y))
+    for i in neighbors:
+        print(Y[np.where(neighbors == i)])
 
 
 class KNN:
@@ -55,6 +89,24 @@ def main(args):
     train_x = train_x.numpy()
     train_y = np.array(train_y)
 
+    unique_y = np.unique(train_y)
+    label_y = unique_y[2]
+
+    imgsList = []
+    for x, y in zip(train_x, train_y):
+        if y == label_y:
+            imgsList.append(x)
+        if len(imgsList) == 16:
+            break
+
+    imgs = np.array(imgsList)
+    _, axs = plt.subplots(4, 4, figsize=(8, 6))
+    axs = axs.flatten()
+    for img, ax in zip(imgs, axs):
+        ax.imshow(np.squeeze(img))
+    plt.show()
+
+    # n = find_neighbors(train_x[0], train_x, 4)
     # TODO: Load and evaluate the classifier for different k
 
     # TODO: Plot results
@@ -62,13 +114,14 @@ def main(args):
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser(description='This script computes cross validation scores for a kNN classifier.')
 
     parser.add_argument('--folds', '-m', type=int, default=5,
                         help='The number of folds that the data is partitioned in for cross validation.')
     parser.add_argument('--train-data', type=str, default=DEFAULT_ROOT,
                         help='Directory in which the training data and the corresponding labels are located.')
-    parser.add_argument('--k', '-k', type=int, default=list(range(1, 11))+[15], nargs='+',
+    parser.add_argument('--k', '-k', type=int, default=list(range(1, 11)) + [15], nargs='+',
                         help='The k values that should be evaluated.')
 
     args = parser.parse_args()
