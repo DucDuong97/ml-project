882958e942c67461479051ec8f1ed170b588e3c2
Duy Nguyen Dinh <mcaneway8469@gmail.com>
2021-11-01 19:08:54 +0100

    Adding initial nn-model, exceeding 90% acc across all size of samples

--- src/task1/dnn.py
+++ src/task1/dnn.py
@@ -1,12 +1,44 @@
 # imports a getter for the StrangeSymbol Dataset loader and the test data tensor
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
 from dataset import get_strange_symbols_train_loader, get_strange_symbols_test_data
 
 if __name__ == '__main__':
     # executing this prepares a loader, which you can iterate to access the data
     trainloader = get_strange_symbols_train_loader(batch_size=128)
 
+
+    # TODO
+    # Now it's up to you to define the network and use the data to train it.
+    class DNN(nn.Module):
+        def __init__(self, input_layer, output_layer):
+            super(DNN, self).__init__()
+            self.fc1 = nn.Linear(input_layer, 256)
+            self.fc2 = nn.Linear(256, output_layer)
+
+        def forward(self, x):
+            x = F.relu(self.fc1(x))
+            x = self.fc2(x)
+            return x
+
+    # Hyperparameters
+    input_size = 784
+    output_size = 15
+    learning_rate = 0.001
+    batch_size = 128
+    num_epoch = 8
+
+    # Create DNN-instance
+    model = DNN(input_size, output_size)
+    # Loss function
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
     # we want to fully iterate the loader multiple times, called epochs, to train successfully
-    for epoch in range(0):
+    for epoch in range(num_epoch):
         # here we fully iterate the loader each time
         for i, data in enumerate(trainloader):
             i = i  # i is just a counter you may use for logging purposes or such
@@ -15,10 +47,42 @@ if __name__ == '__main__':
             # As you may notice, img is of shape n x 1 x height x width, which means a batch of n matrices.
             # But fully connected neural network layers are designed to process vectors. You need to take care of that!
             # Also libraries like matplotlib usually expect images to be of shape height x width x channels.
-            print(img.shape)
+            img = img.reshape(img.shape[0], -1)
 
-    # TODO
-    # Now it's up to you to define the network and use the data to train it.
+            # forward propagation
+            scores = model(img.float())
+            loss = loss_func(scores, label)
+
+            # zero previous gradients
+            optimizer.zero_grad()
+
+            # back-propagation
+            loss.backward()
+
+            # adam-optimizer's step
+            optimizer.step()
+
+    def checkAccuracy(loader, model):
+        num_correct = 0
+        num_sample = 0
+        model.eval()
+        with torch.no_grad():
+            for x, y in loader:
+                x = x.reshape(x.shape[0], -1)
+                scores = model(x.float())
+                _, preds = scores.max(1)
+                num_correct += (preds == y).sum()
+                num_sample += preds.size(0)
+
+                print("Total scanned samples: ", num_sample)
+                print("Correct samples: ", num_correct)
+                print("Accuracy: ", float(num_correct) / float(num_sample))
+                print("----------------------------------------------------------")
+
+        model.train()
+
+checkAccuracy(trainloader, model)
+
     # The code above is just given as a hint, you may change or adapt it.
     # Nevertheless, you are recommended to use the above loader with some batch size of choice.
 
