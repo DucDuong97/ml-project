d0e346f559763a58868d47c26dacdff35e587f88
DUCDUONG <duong.tracer@gmail.com>
2021-12-12 04:55:47 +0700

    implement ridge regression

--- src/task2/regression.py
+++ src/task2/regression.py
@@ -12,10 +12,15 @@ class RidgeRegression:
         self.C = C
 
     def fit(self, X, y):
-        raise NotImplementedError('TODO')
+        n, d = X.shape
+        XTX = np.matmul(X.T,X)
+        XTy = np.matmul(X.T,y)
+        CId = np.eye(d)/self.C
+        print(XTX)
+        self.weight = np.linalg.inv(XTX + CId) @ XTy
 
     def predict(self, X):
-        raise NotImplementedError('TODO')
+        return X @ self.weight
 
 
 #TODO: 2c
@@ -35,7 +40,7 @@ def projection(data):
     #TODO: 2b
     # remember to except the 'point' column
     # it should return an numpy 1d-array
-    pass
+    return data
 
 
 def transform(data):
@@ -53,7 +58,7 @@ def forward_stepwise_selection(data, points, k=5):
 
 
 if __name__ == '__main__':
-    setup_matplotlib()
+    # setup_matplotlib()
 
     # return a dict that values are a list
     data = vectorized_data()
@@ -61,18 +66,18 @@ if __name__ == '__main__':
     print(points)
 
     #TODO: 2b, 2c
-    for col, data in data.items():
-        train_x = projection(data)
+    for col, col_data in data.items():
+        train_x = projection(col_data)
         # plot projected data
         clf = RidgeRegression()
         clf.fit(train_x, points)
         # plot clf.predict
         clf = RidgeRegressionBias()
-        clf.fit(train_x, points)
+        # clf.fit(train_x, points)
         # plot clf.predict
 
     #TODO: 2d, should be smaller than 6.3
-    print(f'Acc: {cross_validation(RidgeRegression(), transform(data), points,metric=mean_sqrt_err)}')
+    # print(f'Acc: {cross_validation(RidgeRegression(), transform(data), points,metric=mean_sqrt_err)}')
 
     #TODO: 2f
-    forward_stepwise_selection(data, points)
+    # forward_stepwise_selection(data, points)
