41d74c59bcc37c4c77c0c3b40d3c6d81d54620b2
DUCDUONG <duong.tracer@gmail.com>
2022-01-23 02:25:28 +0700

    implement value iteration

--- src/task3/tabular_rl.py
+++ src/task3/tabular_rl.py
@@ -6,6 +6,7 @@ import gridworld as gw
 
 import gridworld_vis as gv
 import matplotlib.pyplot as plt
+import seaborn as sb
 
 
 def sarsa(world, ep_num, prob, step_size=1, max_step=80):
@@ -127,6 +128,77 @@ def action_num_to_text(action):
         text = 'down'
     return text
 
+
+def value_iteration(world, discount=1.0, theta=1e-9, max_iter=1e4):
+    # Initialize state-value function with zeros for each environment state
+    print('---------------------------------')
+    print('STARTING VALUE ITERATION')
+    V = np.zeros(world.grid.shape)
+    for i in range(int(max_iter)):
+        # Early stopping condition
+        delta = 0
+        # Update each state
+        for state in world.grid.flatten():
+            if isinstance(state,gw.WallCell): continue
+            # Do a one-step lookahead to calculate state-action values
+            action_values = one_step_lookahead(world, state, V, discount)
+            # Select best action to perform based on the highest state-action value
+            best_action_value = np.max(action_values)
+            # Calculate change in value
+            delta = max(delta, np.abs(V[state.y,state.x] - best_action_value))
+            # Update the value function for current state
+            V[state.y,state.x] = best_action_value
+            # Check if we can stop
+        if delta < theta:
+                print(f'Value-iteration converged at iteration#{i}.')
+                break
+        print(f'Iter {i}, Delta {delta}')
+    # Create a deterministic policy using the optimal value function
+    policy = np.zeros((16, 16, 4))
+    for state in world.grid.flatten():
+            if isinstance(state,gw.WallCell): continue
+            # One step lookahead to find the best action for this state
+            action_values = one_step_lookahead(world, state, V, discount)
+            # Select best action based on the highest state-action value
+            best_action = np.argmax(action_values)
+            # Update the policy to perform a better action at a current state
+            policy[state.x, state.y, best_action] = 1.0
+    return policy, V
+
+
+def one_step_lookahead(world, state, V, discount_factor):
+    nA = 4
+    action_values = np.zeros(nA)
+    for action_num in range(nA):
+        action = action_num_to_text(action_num)
+        proposed_state = state.step(action)
+        if proposed_state.allow_enter(state, action):
+            next_state = proposed_state
+        else:
+            next_state = state
+        reward = world.reward_class.reward_f(state, action, next_state)
+        p = world.p(next_state, reward, state, action)
+        action_values[action_num] += p * (reward + discount_factor * V[next_state.y, next_state.x])
+    return action_values
+
+
+def walk_with_policy(world, policy, max_step=60):
+    done = False
+    current_step = 0
+    moves = []
+    world.reset()
+    S = world.current_state
+    while not done and current_step < max_step:
+        A = choose_action(world, policy, 0)
+        new_state, reward, done = world.step(A[0])
+        moves.append((new_state.x-S.x,new_state.y-S.y))
+        current_step += 1
+        # Step 4
+        S = new_state
+    return moves
+
+
+
 def visualize_gridworld(world,actions):
     def tile2classes(x, y):
         cell = world.get_state(x,15 - y)
@@ -157,7 +229,13 @@ def plot_ret_eps(returns):
 
 if __name__ == '__main__':
     world = gw.World.load_from_file('world.json')
-    # actions = sarsa(world,10000, 0.1, step_size=0.5, max_step=60)
-    actions, returns = q_learn(world,10000, 0.01, step_size=1, max_step=60)
-    visualize_gridworld(world,actions)
-    plot_ret_eps(returns)
+    # actions, returns = sarsa(world,10000, 0.1, step_size=0.5, max_step=60)
+    # actions, returns = q_learn(world,10000, 0.01, step_size=1, max_step=60)
+    # visualize_gridworld(world,actions)
+    # plot_ret_eps(returns)
+
+    # world.reward_class = gw.ThirdReward
+    policy,V = value_iteration(world)
+    sb.heatmap(V,annot=True)
+    plt.show()
+    # visualize_gridworld(world,walk_with_policy(world, policy))
