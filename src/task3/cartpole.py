9abe8907cf82fc3ea81995e4c2ed52d992847efb
unknown <mcaneway8469@gmail.com>
2022-01-28 17:58:26 +0100

    Commit before pulling

--- src/task3/cartpole.py
+++ src/task3/cartpole.py
@@ -53,15 +53,22 @@ def interactive_cartpole():
     env.close()
 
 
-num_bucket_x = 3
-num_bucket_delta_x = 3
-num_bucket_theta = 12
-num_bucket_delta_theta = 6
+num_bucket_x = 1
+num_bucket_delta_x = 1
+num_bucket_theta = 6
+num_bucket_delta_theta = 3
+
+# Define bounds for the states
+env.observation_space.low[3] = -0.82
+env.observation_space.high[3] = 0.82
 value_bounds = list(zip(env.observation_space.low, env.observation_space.high))
+value_bounds[1] = (-9.6, 9.6)
+value_bounds[3] = (-0.82, 0.82)
 
 
 def q_learn_cartpole(num_ep, prob, step_size=1, max_step=500):
-    Q = np.zeros((num_bucket_x, num_bucket_delta_x, num_bucket_theta, num_bucket_delta_theta, 2))
+    # Q = np.zeros((num_bucket_x, num_bucket_delta_x, num_bucket_theta, num_bucket_delta_theta, 2))
+    Q = np.zeros((num_bucket_theta, num_bucket_delta_theta, 2))
     returns = []
     moves = []
 
@@ -71,7 +78,7 @@ def q_learn_cartpole(num_ep, prob, step_size=1, max_step=500):
         done = False
 
         env.reset()
-        env.render()
+        #env.render()
         S = env.state
         current_step = 0
         moves = []
@@ -82,22 +89,23 @@ def q_learn_cartpole(num_ep, prob, step_size=1, max_step=500):
             # Step 2
             new_state, reward, done, info = env.step(A)
 
+            #print(new_state)
             if done:
-                print(current_step)
+                print(f"Reward: {current_step}")
                 break
 
             # Step 3: Update Q
-            lookup_values = Q[  map_to_bucket(new_state[0], value_bounds[0], num_bucket_x),
-                                map_to_bucket(new_state[1], value_bounds[1], num_bucket_delta_x),
+            lookup_values = Q[  #map_to_bucket(new_state[0], value_bounds[0], num_bucket_x),
+                                #map_to_bucket(new_state[1], value_bounds[1], num_bucket_delta_x),
                                 map_to_bucket(new_state[2], value_bounds[2], num_bucket_theta),
                                 map_to_bucket(new_state[3], value_bounds[3], num_bucket_delta_theta), :]
             max_arg = max(lookup_values)
-            Q[map_to_bucket(S[0], value_bounds[0], num_bucket_x),
-              map_to_bucket(S[1], value_bounds[1], num_bucket_delta_x),
+            Q[#map_to_bucket(S[0], value_bounds[0], num_bucket_x),
+              #map_to_bucket(S[1], value_bounds[1], num_bucket_delta_x),
               map_to_bucket(S[2], value_bounds[2], num_bucket_theta),
               map_to_bucket(S[3], value_bounds[3], num_bucket_delta_theta), A] \
-                += step_size * (reward + max_arg - Q[map_to_bucket(S[0], value_bounds[0], num_bucket_x),
-                                                     map_to_bucket(S[1], value_bounds[1], num_bucket_delta_x),
+                += step_size * (reward + max_arg - Q[#map_to_bucket(S[0], value_bounds[0], num_bucket_x),
+                                                     #map_to_bucket(S[1], value_bounds[1], num_bucket_delta_x),
                                                      map_to_bucket(S[2], value_bounds[2], num_bucket_theta),
                                                      map_to_bucket(S[3], value_bounds[3], num_bucket_delta_theta), A])
             # Retriving observed data and print info
@@ -106,7 +114,8 @@ def q_learn_cartpole(num_ep, prob, step_size=1, max_step=500):
             current_step += 1
             # Step 4
             S = new_state
-        env.close()
+        print(moves)
+        #env.close()
     return Q
 
 
@@ -115,8 +124,8 @@ def choose_action(env, Q, prob):
     if choice == "random_action":
         return env.action_space.sample()
     else:
-        lookup_values = Q[  map_to_bucket(env.state[0], value_bounds[0], num_bucket_x),
-                            map_to_bucket(env.state[1], value_bounds[1], num_bucket_delta_x),
+        lookup_values = Q[  #map_to_bucket(env.state[0], value_bounds[0], num_bucket_x),
+                            #map_to_bucket(env.state[1], value_bounds[1], num_bucket_delta_x),
                             map_to_bucket(env.state[2], value_bounds[2], num_bucket_theta),
                             map_to_bucket(env.state[3], value_bounds[3], num_bucket_delta_theta), :]
         max_lookup_value = max(lookup_values)
@@ -124,13 +133,16 @@ def choose_action(env, Q, prob):
 
 
 def map_to_bucket(value, bounds, num_bucket):
+    #print(value)
+    #print(bounds)
     bucket_size = (bounds[1] - bounds[0]) / num_bucket
-    if value == bounds[0]:
+    if value <= bounds[0]:
         return 0
-    elif value == bounds[1]:
-        num_bucket - 1
-    return math.floor(value / bucket_size)
+    elif value >= bounds[1]:
+        return num_bucket - 1
+    return math.floor((value - bounds[0]) / bucket_size)
 
 
 if __name__ == '__main__':
-    print(q_learn_cartpole(800, 0.01))
+    #print(q_learn_cartpole(800, 0.05))
+    print(1.0 - math.log10((200+1)/25.0))
